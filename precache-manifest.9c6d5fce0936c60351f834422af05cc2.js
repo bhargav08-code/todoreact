15cbd0153207c58b78cd23b36fbe0622526a8028
bhargav08-code <bhargavgarge1828@gmail.com>
2022-09-13 23:29:10 +0530

    Updates

---
+++ precache-manifest.9c6d5fce0936c60351f834422af05cc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "127148e9571cdb03b4b2",
+    "url": "/todoreact/static/css/main.92469373.chunk.css"
+  },
+  {
+    "revision": "127148e9571cdb03b4b2",
+    "url": "/todoreact/static/js/main.127148e9.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/todoreact/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "cb1335819c6f651c03ba",
+    "url": "/todoreact/static/js/2.cb133581.chunk.js"
+  },
+  {
+    "revision": "ef3309a7588ab526d0f6",
+    "url": "/todoreact/static/js/runtime~main.ef3309a7.js"
+  },
+  {
+    "revision": "05378cb1d0060d8b2f0032b0b1601086",
+    "url": "/todoreact/static/media/trianglify-lowres.05378cb1.png"
+  },
+  {
+    "revision": "a4b5cd0c81acd1c97e6df967ae13160b",
+    "url": "/todoreact/index.html"
+  }
+];
